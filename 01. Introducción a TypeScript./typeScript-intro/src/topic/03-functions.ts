f16c2b2e9f24a3e8ca085f73c6e98c0296754d3d
dsantos <danilosantosalvarado@gmail.com>
2024-02-28 17:06:54 +0000

    videos funciones

--- 01. Introducción a TypeScript./typeScript-intro/src/topic/03-functions.ts
+++ 01. Introducción a TypeScript./typeScript-intro/src/topic/03-functions.ts
@@ -2,9 +2,35 @@
  // no recibe el tipo de dato 'any' any = no se que tipo de dato es, para que no genere ningun error
  // en la variable 'b' es necesary agregar el type en ':any' explicitamente por buena practica.
 
-function addNumber(a:number, b:number){
+function addNumber(a:number, b:number):number{
     return a + b;
 }
 
 
-const result: number = addNumber(2,5);
+const result: number = addNumber(2 ,5);
+
+console.log({result}); //impime la variable como un  objet{} , para ver el consola el nombre de la variable.
+
+
+
+// funcion en arrow
+
+const addNumberArrow = (a: number, b: number): number => {
+    return a + b;
+}
+
+const addNumberArrow2 = (a: number, b: number): string => {
+    return `${ a + b}`;
+}
+
+
+function multiply(firstNumber: number, secondNumber?: number, base: number = 2){
+    return firstNumber * base;
+}
+
+
+
+const result3 : number = addNumberArrow(2,3);
+const result4 : String = addNumberArrow2(2,3);
+const multiplyResult = multiply(5);
+console.log({result3, result4, multiplyResult});
